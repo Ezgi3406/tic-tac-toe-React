d4e0ddea9788bf47c6c5eca3eaa7161eb5274901
Ezgi3406 <ezgigunes3406@gmail.com>
2021-09-20 23:37:43 +0100

    aaaa

---
+++ build/precache-manifest.e6cff76a1fcf7d40a51ab343a9d0a832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76924a2b83c887653e0442c8fc4ce7ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0616bd903fe587f6b985",
+    "url": "/static/css/main.5a4ac6da.chunk.css"
+  },
+  {
+    "revision": "9a41953fe7320979b8d0",
+    "url": "/static/js/2.3d8fdf42.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.3d8fdf42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0616bd903fe587f6b985",
+    "url": "/static/js/main.c2394f4f.chunk.js"
+  },
+  {
+    "revision": "2bf7b979d2cbe31b2241",
+    "url": "/static/js/runtime-main.92e60192.js"
+  }
+]);
